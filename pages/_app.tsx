bd77fef553d0750e295e3476ffcdb7148d60a08a
chrishj59 <chrishj@btinternet.com>
2022-11-25 14:22:27 +0000

    Product loaded

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,33 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import 'primeflex/primeflex.css';
+import 'primeicons/primeicons.css';
+import 'primereact/resources/primereact.min.css';
+import 'primereact/resources/themes/md-light-deeppurple/theme.css';
 
+import { UserProvider } from '@auth0/nextjs-auth0';
+import Head from 'next/head';
+
+import Layout from '../components/Layout';
+
+import type { AppProps } from 'next/app';
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	console.log('Component');
+	console.log(Component);
+	console.log('pageProps');
+	console.log(pageProps['menuItems']);
+	const menuItems = pageProps['menuItems'];
+	return (
+		<>
+			<Head>
+				<title>Saintly Sinners</title>
+			</Head>
+			<UserProvider>
+				<Layout menuItems={menuItems}>
+					<Component {...pageProps} />
+				</Layout>
+			</UserProvider>
+		</>
+	);
 }
 
-export default MyApp
+export default MyApp;
