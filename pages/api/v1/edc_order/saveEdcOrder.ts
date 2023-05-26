a098b5982b67249794477f2c4024d6bc57e0c037
chrishj59 <chrishj@btinternet.com>
2023-05-26 13:01:17 +0100

    remove logging from api/saveEdcOrder

--- pages/api/v1/edc_order/saveEdcOrder.ts
+++ pages/api/v1/edc_order/saveEdcOrder.ts
@@ -5,20 +5,16 @@ export default async function handler(
 	_req: NextApiRequest,
 	_res: NextApiResponse<any>
 ) {
-	console.log(`saveOrderApi req body ${JSON.stringify(_req.body, null, 2)}`);
-	console.log('saveEdcOrder called');
 	const order = _req.body;
 	const url = process.env.EDC_API_BASEURL + '/customerOrder';
-	console.log(`url to be called ${url}`);
 
 	try {
 		const { data } = await axios.post(url, order);
-		console.log(`data: ${JSON.stringify(data)}`);
 
 		_res.send(data);
 	} catch (err) {
 		if (axios.isAxiosError(err)) {
-			console.log(err.status);
+			console.error(err.status);
 			console.error(err.response);
 			// Do something with this error...
 			_res.status(500);
