68b2ebc82f13cbbe360b0c77134a320655d97d67
chrishj59 <chrishj@btinternet.com>
2024-11-28 06:17:56 +0000

    Feat: add ronots.txt and siteMap.xml

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,44 +1,78 @@
-//
-import path from 'path';
-import fs from 'fs';
+// //
+// // import path from 'path';
+// //import fs from 'fs';
 
-const extensions = ['tsx', 'mdx'];
-const baseUrl = 'https://saintlySinners.co.uk';
-const baseDir = 'app';
+import { Languages } from 'next/dist/lib/metadata/types/alternative-urls-types';
 
-function getRoutesFromDir(fullPath: string, prefix = ''): string[] {
-	const entries = fs.readdirSync(fullPath, { withFileTypes: true });
-	return entries.reduce((routes, entry) => {
-		if (entry.isDirectory()) {
-			const newPrefix = `${prefix}/${entry.name}`;
-			if (
-				extensions.some((ext) =>
-					fs.existsSync(path.join(fullPath, entry.name, `page.${ext}`))
-				)
-			) {
-				routes.push(newPrefix);
-			}
-			// Recursively get routes from subdirectories
-			const subDir = path.join(fullPath, entry.name);
-			return routes.concat(getRoutesFromDir(subDir, newPrefix));
-		}
-		return routes;
-	}, [] as string[]);
-}
+// const extensions = ['tsx', 'mdx'];
+// const baseUrl = 'https://saintlySinners.co.uk';
+// const baseDir = 'app';
 
-function getRoutes() {
-	const fullPath = path.join(process.cwd(), baseDir);
-	const routes = getRoutesFromDir(fullPath);
-	return routes.map((route) => {
-		return {
-			url: `${baseUrl}${route}`,
+// function getRoutesFromDir(fullPath: string, prefix = ''): string[] {
+// 	const entries = fs.readdirSync(fullPath, { withFileTypes: true });
+// 	return entries.reduce((routes, entry) => {
+// 		if (entry.isDirectory()) {
+// 			const newPrefix = `${prefix}/${entry.name}`;
+// 			if (
+// 				extensions.some((ext) =>
+// 					fs.existsSync(path.join(fullPath, entry.name, `page.${ext}`))
+// 				)
+// 			) {
+// 				routes.push(newPrefix);
+// 			}
+// 			// Recursively get routes from subdirectories
+// 			const subDir = path.join(fullPath, entry.name);
+// 			return routes.concat(getRoutesFromDir(subDir, newPrefix));
+// 		}
+// 		return routes;
+// 	}, [] as string[]);
+// }
+
+// function getRoutes() {
+// 	const fullPath = path.join(process.cwd(), baseDir);
+// 	const routes = getRoutesFromDir(fullPath);
+// 	return routes.map((route) => {
+// 		return {
+// 			url: `${baseUrl}${route}`,
+// 			lastModified: new Date(),
+// 			changeFrequency: 'weekly',
+// 			priority: 1.0,
+// 		};
+// 	});
+// }
+
+// export default function sitemap() {
+// 	return getRoutes();
+// }
+
+type Sitemap = Array<{
+	url: string;
+	lastModified?: string | Date;
+	changeFrequency?:
+		| 'always'
+		| 'hourly'
+		| 'daily'
+		| 'weekly'
+		| 'monthly'
+		| 'yearly'
+		| 'never';
+	priority?: number;
+	alternates?: {
+		languages?: Languages<string>;
+	};
+}>;
+
+import type { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+	return [
+		{
+			url: 'https://saintlySinners.co.uk',
 			lastModified: new Date(),
-			changeFrequency: 'weekly',
-			priority: 1.0,
-		};
-	});
-}
-
-export default function sitemap() {
-	return getRoutes();
+		},
+		{
+			url: 'https://saintlySinners.co.uk/support/frequent-questions',
+			lastModified: new Date(),
+		},
+	];
 }
